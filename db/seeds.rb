6ffad638a39c4dfb928a31f602f28423c3d8d1c0
Adrien Jones <tjones69@memphis.edu>
2024-04-18 16:25:17 -0500

    Implemented index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+paintings = [["Anxiety", 40.00, "This painting conveys the anguish of living with extreme anxiety."],
+  ["Bloom", 700.00, "An explosion of Spring."],
+  ["Obsession", 40.00, "Shimmering gold dust with a dark side"],
+  ["Wavelength", 500.00, "The shaky proof someone was here."]]
+
+paintings.each do |p|
+  Product.create(name: p[0], price: p[1], description: p[2])
+end
